57a9680b04bb8c9270d671ecf0c3bf1210ae14b0
SaiBolam <sbolam@outlook.com>
2025-07-11 18:20:48 +0100

    update the latest with no test-data-module
    removed test-data-module

--- test-data-module/test-data/stream_endTime_calc.js
+++
@@ -1,7 +0,0 @@
-// this function is used to calculate the end times for a stream and then used in a test case 
-// to validate the end time of a live stream is past the current time
-function streamEndTimeCalcISOString() {
-  return new Date(Date.now() - 2 * 60 * 60 * 1000).toISOString();
-}
-
-module.exports = { streamEndTimeCalcISOString };
